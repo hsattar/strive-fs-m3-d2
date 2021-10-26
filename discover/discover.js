50de15938c1ac0f8e87157299c4743df2a8bf381
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-26 15:22:20 +0100

    display 3 artists

--- discover/discover.js
+++ discover/discover.js
@@ -1,23 +1,34 @@
 const myKey = config.MY_KEY;
 
-fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-		"x-rapidapi-key": myKey
-    } })
-    .then(response => response.json())
-    .then(data => {
-        const songs = data.data
-        console.log(data.data)
-        displaySongs(songs)
-    })
-    .catch(error => console.log(error))
+const artists = ['eminem', 'ariana grande', 'ed sheeran']
 
-const displaySongs = songs => {
+window.onload= () => {
+
+artists.forEach(artist => {
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artist}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+            "x-rapidapi-key": myKey
+        } })
+        .then(response => response.json())
+        .then(data => {
+            const songs = data.data
+            console.log(data.data)
+            displaySongs(songs, artist)
+        })
+        .catch(error => console.log(error))
+})
+
+
+const displaySongs = (songs, artist) => {
     const container = document.querySelector('.api-data')
     const row = document.createElement('div')
     row.className = 'row d-flex justify-content-center'
+    const artistTitle = document.createElement('h3')
+    artistTitle.className = 'col-12 text-center my-3'
+    artistTitle.innerText = artist.toUpperCase()
+    row.appendChild(artistTitle)
     songs.forEach(song => {
         const card = document.createElement('div')
         card.className = 'hp-subhero-card col-12 col-sm-6 col-md-4 col-lg-3 m-2 rounded'
@@ -31,4 +42,6 @@ const displaySongs = songs => {
         row.appendChild(card)
     })
     container.appendChild(row)
+}
+
 }
