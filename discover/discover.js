d0f0fc0df911495c16a1bc3776be7efb8281dff2
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-26 14:12:24 +0100

    connected to api

--- discover/discover.js
+++ discover/discover.js
@@ -1,1 +1,19 @@
-const mykey = config.MY_KEY;
+const myKey = config.MY_KEY;
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+		"x-rapidapi-key": myKey
+    } })
+    .then(response => response.json())
+    .then(data => {
+        const songs = data.data
+        console.log(data.data)
+        displaySongs(songs)
+    })
+    .catch(error => console.log(error))
+
+const displaySongs = songs => {
+    const container = document.querySelector('.api-data')
+}
