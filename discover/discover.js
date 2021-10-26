c45b9d863bc643bb3cf604f07d13c65060be281c
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-26 14:27:32 +0100

    displaying data

--- discover/discover.js
+++ discover/discover.js
@@ -16,4 +16,19 @@ fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
 
 const displaySongs = songs => {
     const container = document.querySelector('.api-data')
+    const row = document.createElement('div')
+    row.className = 'row d-flex justify-content-center'
+    songs.forEach(song => {
+        const card = document.createElement('div')
+        card.className = 'hp-subhero-card col-12 col-sm-6 col-md-4 col-lg-3 m-2 rounded'
+        card.innerHTML = `
+        <img src=${song.album.cover_xl} class="card-img-top pt-2 img-fluid" alt="...">
+        <div class="card-body">
+            <p class="hp-subhero-title">${song.title}</p>
+            <p></p>
+            <p class="hp-subhero-subtitle mb-0"></p>
+        </div>`
+        row.appendChild(card)
+    })
+    container.appendChild(row)
 }
